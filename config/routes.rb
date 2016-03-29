3e50c7f6578c94c4b9683c5945b983dcbf21c2ca
Justine Herman <justine.m.h@gmail.com>
2016-03-29 20:22:39 +0000

    Add helper methods, restrict access if not current user

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
   
   get 'reviews/new'
-
   get 'reviews/create'
-
   get 'sessions/new'
-
   get 'sessions/create'
-
   get 'users/new'
-
   get 'users/create'
 
   resources :movies do
@@ -17,6 +12,7 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  root to: 'movies#index'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
